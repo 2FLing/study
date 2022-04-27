3afa04acefa23f3be7c4387eb6a31fbe086be42a
2FLing <349332929yaofu@gmail.com>
2022-04-26 18:31:32 -0700

    rewrite two sum for csci-115 project

--- CSCI-115(data structure and algorithm)/final_project/two_sum(debug).cpp
+++ CSCI-115(data structure and algorithm)/final_project/two_sum(debug).cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <vector>
+#include<random>
+#include<time.h>
 using namespace std;
 bool two_sum(vector<int>, int);
 bool two_sum_evolve(vector<int>, int);
+vector<int>make_v(int size);
 int main()
 {
-    vector<int> v = {1, -2, 3, 3, 6};
-    cout << two_sum_evolve(v, 1);
+    vector<int> v = make_v(1000000);
+    float end, start;
+    v.push_back(-1);
+    v.push_back(3999999);
+    start = clock();
+    cout << two_sum_evolve(v, 3999998)<<endl;
+    end = clock();
+    cout << (end - start) / CLOCKS_PER_SEC << "s" << endl;
     return 0;
 }
 
@@ -27,7 +36,7 @@ bool two_sum(vector<int> v, int sum)
 bool two_sum_evolve(vector<int> v, int target)
 {
     int size = 0, min = v[0], max = v[0];
-    for (int i = 0; i < v.size() - 1; i++) 
+    for (int i = 0; i < v.size() - 1; i++)
     {
         if (v[i] > v[i + 1])
         {
@@ -44,23 +53,23 @@ bool two_sum_evolve(vector<int> v, int target)
                 min = v[i];
         }
     }
-    if (max < 0)        
-    {           
-        size = 1 - min; 
+    if (max < 0)
+    {
+        size = 1 - min;
     }
     else if (min > 0)
     {
-        size = max + 1; 
+        size = max + 1;
     }
     else
-        size = max - min + 1; 
+        size = max - min + 1;
 
-    vector<int> temp(size); 
+    vector<int> temp(size);
 
     for (int i = 0; i < v.size(); i++)
     {
         int still_need = target - v[i];
-        if (still_need > max or still_need < min)
+        if (still_need > max || still_need < min)
         {
             continue;
         }
@@ -70,4 +79,15 @@ bool two_sum_evolve(vector<int> v, int target)
             return true;
     }
     return false;
+}
+vector<int>make_v(int size)
+{
+    vector<int>v;
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        int t = rand() % size-size;
+        v.push_back(t);
+    }
+    return v;
 }
--- CSCI-115(data structure and algorithm)/final_project/two_sums(comments).cpp
+++ CSCI-115(data structure and algorithm)/final_project/two_sums(comments).cpp
@@ -5,8 +5,8 @@ bool two_sum(vector<int>, int);
 bool two_sum_evolve(vector<int>, int);
 int main()
 {
-    vector<int> v = {1, -2, 3, 3, 6};
-    cout << two_sum_evolve(v, 1);
+    vector<int> v = {1, 2, 3, 3, 6};
+    cout << two_sum_evolve(v, 6);
     return 0;
 }
 
@@ -24,9 +24,17 @@ bool two_sum(vector<int> v, int sum)
     }
     return false;
 }
+// we are using a new array to store each number that we went through.
+// the worst case will be we went through the whole given array, still could not find those two numbers
+// therefore, for storing the whole given array, we need the range of the given array
+// which will be [min,max].
+// if the min > 0, then the range will be [0,max]
+// if the max < 0, then the range will be [min,0]
+// if min < 0 and max > 0 then the range will be [min,max]
 bool two_sum_evolve(vector<int> v, int target)
 {
     int size = 0, min = v[0], max = v[0];
+    //find the max and min in the given array
     for (int i = 0; i < v.size() - 1; i++) 
     {
         if (v[i] > v[i + 1])
@@ -45,25 +53,37 @@ bool two_sum_evolve(vector<int> v, int target)
         }
     }
     if (max < 0)        
-    {           
+    {
+        // because min < 0, size = -min + 1, 1 for the 0.                   
         size = 1 - min; 
     }
     else if (min > 0)
     {
+        // 1 for the 0.
         size = max + 1; 
     }
     else
+        // because min < 0, size = -min + max + 1, 1 for the 0.
         size = max - min + 1; 
 
+    // temp is the array used for storing number that went through
     vector<int> temp(size); 
 
     for (int i = 0; i < v.size(); i++)
     {
+        // what number we still need to make number + v[i] = target
         int still_need = target - v[i];
+        // if number > max or number < min, it is absolutely not in the given array.
         if (still_need > max or still_need < min)
         {
             continue;
         }
+        //otherwise check if it exists in those numbers already went through.
+        //temp[still_need - min] == 0 means have not gone through yet
+        //then mark the temp[v[i] - min] = 1 to record we went through v[i].
+        //temp[still_need - min] == 1 means we went through the number we need
+        //in other words, in the given array, it exists a number that number + v[i] = target.
+        // therefore, return true.
         else if (temp[still_need - min] == 0)
             temp[v[i] - min]++;
         else
