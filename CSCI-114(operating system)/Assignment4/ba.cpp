3303a657c7bfea4eb38afcf36e1ecf6ef23945a7
2FLing <349332929yaofu@gmail.com>
2022-04-30 03:43:09 -0700

    csci114 assignment4

--- CSCI-114(operating system)/Assignment4/ba.cpp
+++ CSCI-114(operating system)/Assignment4/ba.cpp
@@ -12,13 +12,14 @@ condition_variable go;
 
 int number_of_threads, resources, available;
 void read_arguments(char**);
-void request(vector<int>&, vector<int>&, int&, int);
-void release(vector<int>&, vector<bool>&, vector<int>, int&, int);
+void request(vector<int>&, int&, int&, int);
+void release(vector<vector<int>>&,vector<int>&, vector<bool>&, int&, int);
 void print_alloc(vector<int>);
-void read_from_file(vector<int>&);
-void banker(vector<int>&, vector<int>&, int&, vector<bool>&, vector<bool>&, vector<int>, int);
+void read_from_file(vector<vector<int>>&);
+void banker(vector<vector<int>>&,vector<int>&, int&, vector<bool>&, vector<bool>&, int);
 bool notify_next(vector<bool>, vector<bool>&, int);
 void write_file(vector<int>);
+// write the content of an array into a txt file
 void write_file(vector<int>v)
 {
     fstream file;
@@ -40,19 +41,24 @@ void read_arguments(char** args)
     resources = stoi(temp);
     available = resources;
 }
-void request(vector<int>& allocation, vector<int>& need, int& available, int index)
+void request(vector<int>& allocation, int& need, int& available, int index)
 {
     available--;
-    need[index]--;
+    need--;
     allocation[index]++;
     print_alloc(allocation);
 }
-void release(vector<int>& allocation, vector<bool>& finish, vector<int> max, int& available, int index)
+void release(vector<vector<int>>&request,vector<int>& allocation, vector<bool>& finish, int& available, int index)
 {
-    finish[index] = true;
-    available += max[index];
+    available += request[index][0];
     allocation[index] = 0;
     print_alloc(allocation);
+    if(request[index].size() == 1)
+        finish[index] = true;
+    else
+    {
+        request[index].erase(request[index].begin(), request[index].begin() + 1);
+    }
 }
 void print_alloc(vector<int> allocation)
 {
@@ -72,22 +78,29 @@ bool notify_next(vector<bool> finish, vector<bool>& ready, int index)
     }
     if (!successfully)
         return successfully;
-    while (finish[temp % number_of_threads])
+    while (finish[temp % number_of_threads])//if the thread is finished already then skip it
         temp++;
-    ready[temp % number_of_threads] = true;
+    ready[temp % number_of_threads] = true; // make next thread to be ready to run
     return successfully;
 }
-void read_from_file(vector<int>& request)
+void read_from_file(vector<vector<int>>& request)
 {
-    fstream file,result;
+    fstream file, result;
     file.open("requests.txt", ios::in);
     if (file.is_open())
     {
         string temp;
+        int index;
         while (getline(file, temp))
         {
+            index = temp[0]-'1';
+            if(index>number_of_threads)
+            {
+                cout<<"The input number of thread is smaller than the current number of thread."<<endl;
+                exit(1);
+            }
             temp = temp.substr(2, temp.size() - 2);
-            request.push_back(stoi(temp));
+            request[index].push_back(stoi(temp));
         }
         file.close();
     }
@@ -97,75 +110,85 @@ void read_from_file(vector<int>& request)
     }
 }
 
-void banker(vector<int>& allocation, vector<int>& need, int& available, vector<bool>& ready, vector<bool>& finish, vector<int> max, int index)
+void banker(vector<vector<int>>&requests,vector<int>& allocation, int& available, vector<bool>& ready, vector<bool>& finish, int index)
 {
     unique_lock<std::mutex> mlock(mtx);
-    if (need[index] == 0)
+    int need = requests[index][0] - allocation[index];
+    // check if the thread needs 0 unit first, if it does then not neccessary to execute rest.
+    if (need == 0) 
         finish[index] = true;
-    while (need[index] != 0)
+    while (need != 0)
     {
-        while (need[index] > available)
+        //if the current thread needs more than available, put it in wait list.
+        while (need > available)
         {
             notify_next(finish, ready, index);
+            ready[index] = false;
             go.notify_one();
             go.wait(mlock);
         }
         request(allocation, need, available, index);
-        if (need[index] == 0)
+        if (need == 0)
         {
-            release(allocation, finish, max, available, index);
+            release(requests,allocation, finish, available, index);
+            //if there is still request for the current thread, then calculate its next need.
+            if(!finish[index])
+                need = requests[index][0] - allocation[index];
+            ready[index] = false;
+            //if notify next fail, that means all threads got satisfied, so break the loop.
             if (!notify_next(finish, ready, index))
                 break;
+            //if there is still thread not be satisfied yet than notify next to be ready.
             go.notify_one();
-            break;
+            //if the current thread is finished then break the loop.
+            if (finish[index])
+                break;
+            //if the current thread is not finished yet then it has to wait for the next turn.
+            while (!ready[index])
+                go.wait(mlock);
         }
-        ready[index] = false;
-        notify_next(finish, ready, index);
-        go.notify_one();
-        while (!ready[index])
-            go.wait(mlock);
+        else
+        {
+            ready[index] = false;
+            notify_next(finish, ready, index);
+            go.notify_one();
+            while (!ready[index])
+                go.wait(mlock);
+        }
     }
 }
 int main(int argv, char** args)
 {
-    vector<int> requests;
+    vector<vector<int>> requests;
     vector<thread> thrds;
     fstream file;
-    int count = 0;
-    file.open("result.txt",ios::out);
+    file.open("result.txt", ios::out);
     file.close();
     read_arguments(args);
+    vector<int> allocation(number_of_threads, 0);
+    vector<bool> ready(number_of_threads, false);
+    vector<bool> finish(number_of_threads, false);
+    requests = vector<vector<int>>(number_of_threads);
     read_from_file(requests);
     for (auto ele : requests)
     {
-        if (resources < ele)
+        for(int i=0;i<ele.size();i++)
         {
-            cout << "The require unit is larger than the resourse." << endl;
-            exit(1);
+            if (ele[i] > resources)
+            {
+                cout << "The require unit is larger than the resourse." << endl;
+                exit(1);
+            }
         }
     }
-    int temp = 0;
-    for (int i = 0; i < requests.size(); i += number_of_threads)
+    for (int i = 0; i < requests.size(); i++)
     {
-        vector<int> max, need;
-        vector<int> allocation(number_of_threads, 0);
-        vector<bool> ready(number_of_threads, false);
-        vector<bool> finish(number_of_threads, false);
-        print_alloc(allocation);
-        for (int j = 0; j < number_of_threads; j++)
-        {
-            max.push_back(requests[j + i]);
-            need.push_back(requests[j + i]);
-        }
-        for (int j = 0; j < number_of_threads; j++)
-        {
-            thrds.push_back(thread(ref(banker), ref(allocation), ref(need), ref(available), ref(ready), ref(finish), ref(max), j));
-        }
-        for (int j = 0; j < number_of_threads; j++)
-        {
-            thrds[j].join();
-        }
-        thrds.clear();
+        thrds.push_back(thread(ref(banker), ref(requests),ref(allocation), ref(available), ref(ready), ref(finish), i));
+    }
+    for (int i = 0; i < requests.size(); i++)
+    {
+        thrds[i].join();
     }
     return 0;
 }
+//thrds.push_back(thread(ref(banker), ref(allocation), ref(available), ref(ready), ref(finish), j));
