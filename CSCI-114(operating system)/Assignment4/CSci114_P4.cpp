760f24f92e754883149aaea3198d06da8dcb4399
2FLing <349332929yaofu@gmail.com>
2022-05-01 00:17:19 -0700

    csci114 assignment4

--- CSCI-114(operating system)/Assignment4/CSci114_P4.cpp
+++ CSCI-114(operating system)/Assignment4/CSci114_P4.cpp
@@ -1,3 +1,20 @@
+// I executed my program on cygwin terminal
+// Before execution, you have to convert the cpp file
+// to exe by using g++ CSci114_P4.cpp -o anyname
+// then you will see a anyname.exe in the same folder
+// when you run the exe program you will need the
+// number of threads and the number of the shrared object. For example, if
+// number of thread is 5, and mthe number of the shrared object is 8
+// for running the exe file, you have to put
+// ./(the name you defined for the .exe file) 5 8
+// Then you should see the content of the allocation.
+
+// Sometimes Cygwin will stuck when printing the allocation,
+// but the program is actually good. Therefore, I made a txt file
+// to store the allocation, if the program stuck when printing the
+// allocation, you can go to the folder where the cpp file stored,
+// then you should see the result.txt file, you can check the file for the result.
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,22 +22,21 @@
 #include <vector>
 #include <mutex>
 #include <condition_variable>
-#include <chrono>
 using namespace std;
 mutex mtx;
 condition_variable go;
 
 int number_of_threads, resources, available;
-void read_arguments(char**);
-void request(vector<int>&, int&, int&, int);
-void release(vector<vector<int>>&,vector<int>&, vector<bool>&, int&, int);
+void read_arguments(char **);
+void request(vector<int> &, int &, int &, int);
+void release(vector<vector<int>> &, vector<int> &, vector<bool> &, int &, int);
 void print_alloc(vector<int>);
-void read_from_file(vector<vector<int>>&);
-void banker(vector<vector<int>>&,vector<int>&, int&, vector<bool>&, vector<bool>&, int);
-bool notify_next(vector<bool>, vector<bool>&, int);
+void read_from_file(vector<vector<int>> &);
+void banker(vector<vector<int>> &, vector<int> &, int &, vector<bool> &, vector<bool> &, int);
+bool notify_next(vector<bool>, vector<bool> &, int);
 void write_file(vector<int>);
 // write the content of an array into a txt file
-void write_file(vector<int>v)
+void write_file(vector<int> v)
 {
     fstream file;
     file.open("result.txt", ios::app);
@@ -31,7 +47,7 @@ void write_file(vector<int>v)
     file << endl;
     file.close();
 }
-void read_arguments(char** args)
+void read_arguments(char **args)
 {
     args++;
     string temp = *args;
@@ -41,19 +57,19 @@ void read_arguments(char** args)
     resources = stoi(temp);
     available = resources;
 }
-void request(vector<int>& allocation, int& need, int& available, int index)
+void request(vector<int> &allocation, int &need, int &available, int index)
 {
     available--;
     need--;
     allocation[index]++;
     print_alloc(allocation);
 }
-void release(vector<vector<int>>&request,vector<int>& allocation, vector<bool>& finish, int& available, int index)
+void release(vector<vector<int>> &request, vector<int> &allocation, vector<bool> &finish, int &available, int index)
 {
     available += request[index][0];
     allocation[index] = 0;
     print_alloc(allocation);
-    if(request[index].size() == 1)
+    if (request[index].size() == 1)
         finish[index] = true;
     else
     {
@@ -67,7 +83,7 @@ void print_alloc(vector<int> allocation)
         cout << ele << " ";
     cout << endl;
 }
-bool notify_next(vector<bool> finish, vector<bool>& ready, int index)
+bool notify_next(vector<bool> finish, vector<bool> &ready, int index)
 {
     bool successfully = false;
     int temp = index + 1;
@@ -78,14 +94,14 @@ bool notify_next(vector<bool> finish, vector<bool>& ready, int index)
     }
     if (!successfully)
         return successfully;
-    //if the thread is finished already then skip it
+    // if the thread is finished already then skip it
     while (finish[temp % number_of_threads])
         temp++;
     // make next thread to be ready to run
     ready[temp % number_of_threads] = true;
     return successfully;
 }
-void read_from_file(vector<vector<int>>& request)
+void read_from_file(vector<vector<int>> &request)
 {
     fstream file, result;
     file.open("requests.txt", ios::in);
@@ -95,14 +111,16 @@ void read_from_file(vector<vector<int>>& request)
         int index;
         while (getline(file, temp))
         {
-            index = temp[0]-'1';
-            if(index>number_of_threads)
+            if (temp.size() < 3)
+                break;
+            index = stoi(temp.substr(0, temp.find(" ")));
+            if (index > number_of_threads)
             {
-                cout<<"The input number of thread is smaller than the current number of thread."<<endl;
+                cout << "The input number of thread is smaller than the current number of thread." << endl;
                 exit(1);
             }
-            temp = temp.substr(2, temp.size() - 2);
-            request[index].push_back(stoi(temp));
+            temp = temp.substr(temp.find(" "));
+            request[index - 1].push_back(stoi(temp));
         }
         file.close();
     }
@@ -112,16 +130,16 @@ void read_from_file(vector<vector<int>>& request)
     }
 }
 
-void banker(vector<vector<int>>&requests,vector<int>& allocation, int& available, vector<bool>& ready, vector<bool>& finish, int index)
+void banker(vector<vector<int>> &requests, vector<int> &allocation, int &available, vector<bool> &ready, vector<bool> &finish, int index)
 {
     unique_lock<std::mutex> mlock(mtx);
     int need = requests[index][0] - allocation[index];
     // check if the thread needs 0 unit first, if it does then not neccessary to execute rest.
-    if (need == 0) 
+    if (need == 0)
         finish[index] = true;
     while (need != 0)
     {
-        //if the current thread needs more than available, put it in wait list.
+        // if the current thread needs more than available, put it in wait list.
         while (need > available)
         {
             notify_next(finish, ready, index);
@@ -132,20 +150,20 @@ void banker(vector<vector<int>>&requests,vector<int>& allocation, int& available
         request(allocation, need, available, index);
         if (need == 0)
         {
-            release(requests,allocation, finish, available, index);
-            //if there is still request for the current thread, then calculate its next need.
-            if(!finish[index])
+            release(requests, allocation, finish, available, index);
+            // if there is still request for the current thread, then calculate its next need.
+            if (!finish[index])
                 need = requests[index][0] - allocation[index];
             ready[index] = false;
-            //if notify next fail, that means all threads got satisfied, so break the loop.
+            // if notify next fail, that means all threads got satisfied, so break the loop.
             if (!notify_next(finish, ready, index))
                 break;
-            //if there is still thread not be satisfied yet than notify next to be ready.
+            // if there is still thread not be satisfied yet then notify next to be ready.
             go.notify_one();
-            //if the current thread is finished then break the loop.
+            // if the current thread is finished then break the loop.
             if (finish[index])
                 break;
-            //if the current thread is not finished yet then it has to wait for the next turn.
+            // if the current thread is not finished yet then it has to wait for the next turn.
             while (!ready[index])
                 go.wait(mlock);
         }
@@ -159,7 +177,7 @@ void banker(vector<vector<int>>&requests,vector<int>& allocation, int& available
         }
     }
 }
-int main(int argv, char** args)
+int main(int argv, char **args)
 {
     vector<vector<int>> requests;
     vector<thread> thrds;
@@ -174,7 +192,7 @@ int main(int argv, char** args)
     read_from_file(requests);
     for (auto ele : requests)
     {
-        for(int i=0;i<ele.size();i++)
+        for (int i = 0; i < ele.size(); i++)
         {
             if (ele[i] > resources)
             {
@@ -185,7 +203,7 @@ int main(int argv, char** args)
     }
     for (int i = 0; i < requests.size(); i++)
     {
-        thrds.push_back(thread(ref(banker), ref(requests),ref(allocation), ref(available), ref(ready), ref(finish), i));
+        thrds.push_back(thread(ref(banker), ref(requests), ref(allocation), ref(available), ref(ready), ref(finish), i));
     }
     for (int i = 0; i < requests.size(); i++)
     {
